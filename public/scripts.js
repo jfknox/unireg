4c56531063229ce12244cd3fc23dca41d0851b18
Joseph Knox <jfknox3@gmail.com>
2015-05-20 12:20:49 -0700

    table created, working on form

--- public/scripts.js
+++ public/scripts.js
@@ -1,43 +1,7 @@
-      var CommentBox = React.createClass({
-        render: function() {
-          return (
-            <div className="commentBox">
-              <h1>Hello, world! Fuck you god dammit.</h1>
-
-              <CommentList/>
-              <CommentForm/>
-            </div>
-            
-          );
-        }
-      });
-
-      var CommentList = React.createClass({
-        render: function() {
-          return (
-            <div classname="commentList">
-              Oh, we are just going to sit here and take it
-            </div>
-          );
-        }
-      });
-
-      var CommentForm = React.createClass({
-        render: function() {
-          return (
-            <div classname="CommentForm">
-              another pointless shit
-            </div>
-          );
-        }
-      });
-      
-
-
-
-
-      React.render(
-        <CommentBox />, 
-        document.getElementById('content')
-
-      );
+var Hello = React.createClass({
+    render: function() {
+        return <div>Hello {this.props.name}</div>;
+    }
+});
+ 
+React.render(<Hello name="World" />, document.getElementById('container'));
